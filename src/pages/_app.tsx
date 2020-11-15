2cb732efc12da70c3f2dfb128f922bfd4c42e65a
caioaugustoo <caioamfr@gmail.com>
2020-11-14 23:34:29 -0300

    Tipagem da função

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../styles/global";
+import GlobalStyles, { Container } from "../styles/global";
 import { theme } from "../styles/themes";
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
       <GlobalStyles />
     </ThemeProvider>
   );
