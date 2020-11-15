bc7fab1071494f4dfdbb786c6c5d2bb870122ac9
caioaugustoo <caioamfr@gmail.com>
2020-11-14 23:42:13 -0300

    Fonte adicionada no body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,9 @@ const GlobalStyles = createGlobalStyle`
   :root {
     font-size: 62.5%;
   }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
 `;
 export default GlobalStyles;
 
